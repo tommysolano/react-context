a91928f8163de208c86bcc4760c8f70ef8a983a1
tommy <tommysolano18@hotmail.com>
2021-11-14 20:45:24 -0500

    actualizar el estado de un context

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import { createContext, useContext } from 'react' //importamos las dependencias necesarias para usar context
+import { createContext, useContext, useState } from 'react'
 
+const Context = createContext({valor: false, toggle: () => {}})
 
-const Context = createContext('valor por defecto') //creamos una constante fuera de app y le asignamos createContext
-                                                    // esta funcion puede debe tener un valor por defecto, puede ser un objeto, un array, un numero o un string
+const Provider = ({ children }) => {
+    const [valor, setValor] = useState(false)
+    const value = {
+        valor,
+        toggle: () => setValor(!valor)
+    }
 
-const Provider = ({ children}) => { //un provider es el encargado de pasar el contexto hacia abajo a los componentes hijos
-  return (
-    <Context.Provider value={'mi valor'} >  
-      {children}
-    </Context.Provider>
-  )
-} 
-
-//el Context.Provider siempre necesita de un value o react nos dara error
-
-const Contenido = () => {
-  const ctx = useContext(Context) //pasamos dentro de useContext el contexto que queremos utilizar
-  return (
-    <div>
-      {ctx}
-    </div>
-  )
+    return (
+        <Context.Provider value={value}>
+            {children}
+        </Context.Provider>
+    )
 }
 
-function App() {
-  return (
-    <Provider >
-      <Contenido/>
-    </Provider>
-  );
+const Componente = () => {
+    const {valor, toggle} = useContext(Context)
+
+    return (
+        <div>
+            <label>{valor.toString()}</label>
+            <button onClick={toggle}>Toggle</button>
+        </div>
+    )
 }
 
-//pasamos el componente Provider dentro de app 
-//si no utilizamos la etiqueta provider context usara el valor por defecto y no el value
 
-export default App;
+const App = () => {
+    return (
+        <Provider>
+            <Componente/>
+        </Provider>
+    )
+}
+
+export default App
